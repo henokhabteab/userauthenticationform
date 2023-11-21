6bc2f964ba3577e75826c29437bc7503d49b6163
henokhabteab <habteabhenok1@gmail.com>
2023-11-21 15:37:21 -0500

    update backend

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ db.query(createTableQuery, (queryErr, result) => {
 });
 
 app.post("/Signup", (req, res) => {
-  const sql = "INSERT INTO login (`name`, `email`,`password`) VALUES (?)";
+  const sql = "INSERT INTO users (`name`, `email`,`password`) VALUES (?)";
   const values = [req.body.name, req.body.email, req.body.password];
   db.query(sql, [values], (err, data) => {
     if (err) {
